c0cf3f701717acc719f8f5d73994e6de04fc07b9
Amgaa <enhuushm518@gmail.com>
2024-12-09 13:06:53 +0800

    Create layout

---
+++ src/app/pages/layout/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { RouterModule, RouterOutlet, RouterLink } from '@angular/router';
+import { TooltipModule } from 'primeng/tooltip';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../../services/auth.service';
+import { log } from 'console';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [
+    TooltipModule,
+    RouterModule,
+    RouterOutlet,
+    RouterLink,
+    CommonModule,
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css',
+})
+export class SidebarComponent {
+  constructor(private readonly authService: AuthService) {}
+
+  isAuthenticated: boolean = false;
+  isAdmin: boolean = false;
+  isUser: boolean = false;
+
+  ngOnInit(): void {
+    this.isAuthenticated = this.authService.isAuthenticated();
+    console.log('isAuthenticated', this.isAuthenticated);
+    this.isAdmin = this.authService.isAdmin();
+    this.isUser = this.authService.isUser();
+  }
+}
