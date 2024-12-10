7c5479910e6d40a06fa1e791c322d10da1da0757
Amgaa <enhuushm518@gmail.com>
2024-12-11 02:40:28 +0800

    Added comments to components

--- src/app/components/product-card/product-card.component.ts
+++ src/app/components/product-card/product-card.component.ts
@@ -1,8 +1,17 @@
+/**
+ * ProductCardComponent
+ * This component is responsible for displaying product details in a card format.
+ * It also includes navigation functionality to the product overview page.
+ * 
+ * Author: Enkh-Amgalan G.
+ */
+
 import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-product-card',
   standalone: true,
@@ -13,11 +22,20 @@ import { RouterModule } from '@angular/router';
 export class ProductCardComponent implements OnInit {
   @Input() product: Product = new Product();
   image: any;
+
+  /**
+   * Angular lifecycle hook executed on component initialization.
+   * Sets the `image` property to the first image URL from the product's list of images, if available.
+   */
   ngOnInit(): void {
     this.image = this.product.imageUrls ? this.product.imageUrls[0] : '';
   }
+
   constructor(private router: Router) {}
 
+  /**
+   * Navigates to the product overview page with the product ID in the route.
+   */
   navigateToCheckOutForm(): void {
     this.router.navigate(['/product-over-view', this.product.id]);
   }
